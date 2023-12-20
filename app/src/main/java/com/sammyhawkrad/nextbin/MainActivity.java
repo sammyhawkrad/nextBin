8b86d27cc14043ff55ace90b16ae94b3d38cae97
Samuel Darkwah Manu <manusam14@yahoo.com>
2023-12-20 19:53:39 +0100

    Add map, zoom to user

--- app/src/main/java/com/sammyhawkrad/nextbin/MainActivity.java
+++ app/src/main/java/com/sammyhawkrad/nextbin/MainActivity.java
@@ -2,11 +2,12 @@ package com.sammyhawkrad.nextbin;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.graphics.Color;
-import android.os.AsyncTask;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.Toast;
 
 import androidx.activity.result.ActivityResultLauncher;
@@ -14,24 +15,16 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.maps.android.data.geojson.GeoJsonLayer;
-import com.google.maps.android.data.geojson.GeoJsonLineStringStyle;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap gMap;
+    private LocationManager locationManager;
     ActivityResultLauncher<String[]> locationPermissionRequest;
 
     @SuppressLint("MissingPermission")
@@ -42,11 +35,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.map);
 
         mapFragment.getMapAsync(this);
 
+        // Request location permissions
         locationPermissionRequest = registerForActivityResult(
                 new ActivityResultContracts.RequestMultiplePermissions(), result -> {
                     Boolean fineLocationGranted = result.getOrDefault(Manifest.permission.ACCESS_FINE_LOCATION, false);
@@ -55,7 +51,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     if ((fineLocationGranted != null && fineLocationGranted) || (coarseLocationGranted != null && coarseLocationGranted)) {
                         gMap.setMyLocationEnabled(true);
                         gMap.getUiSettings().setMyLocationButtonEnabled(true);
-
                     } else {
                         Toast.makeText(this,
                                 "Location cannot be obtained due to missing permission.",
@@ -65,67 +60,65 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         );
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap map) {
         gMap = map;
 
         gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
         gMap.getUiSettings().setZoomControlsEnabled(true);
+
+        // Check if GPS is enabled
+        if (isGPSEnabled()) {
+            // Request location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 10000, 10, locationListener);
+        } else {
+            // Prompt the user to enable GPS
+            Toast.makeText(this, "Please enable GPS", Toast.LENGTH_SHORT).show();
+        }
 
         String[] PERMISSIONS = {
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.ACCESS_COARSE_LOCATION
         };
         locationPermissionRequest.launch(PERMISSIONS);
-
-        // zoom to user position:
-        gMap.setOnMyLocationChangeListener(location -> gMap.animateCamera(
-                com.google.android.gms.maps.CameraUpdateFactory.newLatLngZoom(
-                        new com.google.android.gms.maps.model.LatLng(location.getLatitude(),
-                                location.getLongitude()), 16.0f)));
     }
 
-    private class DownloadGeoJsonFile extends AsyncTask<String, Void, GeoJsonLayer> {
-
+    private LocationListener locationListener = new LocationListener() {
         @Override
-        protected GeoJsonLayer doInBackground(String... params) {
-            try {
-                InputStream stream = new URL(params[0]).openStream();
+        public void onLocationChanged(Location location) {
+            // Called when the location has changed
+            if (location != null) {
+                double userLatitude = location.getLatitude();
+                double userLongitude = location.getLongitude();
 
-                String line;
-                StringBuilder result = new StringBuilder();
-                BufferedReader reader = new BufferedReader(
-                        new InputStreamReader(stream));
+                // Animate the camera to the user's location
+                LatLng userLatLng = new LatLng(userLatitude, userLongitude);
+                gMap.animateCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 15.0f));
 
-                while ((line = reader.readLine()) != null) {
-                    result.append(line);
-                }
-
-                reader.close();
-                stream.close();
-
-                return new GeoJsonLayer(gMap, new JSONObject(result.toString()));
-            } catch (IOException e) {
-                Log.e("mLogTag", "GeoJSON file could not be read");
-            } catch (JSONException e) {
-                Log.e("mLogTag",
-                        "GeoJSON file could not be converted to a JSONObject");
+                // Stop requesting location updates
+                locationManager.removeUpdates(locationListener);
             }
-            return null;
         }
 
         @Override
-        protected void onPostExecute(GeoJsonLayer layer) {
-            if (layer != null) {
-                GeoJsonLineStringStyle lineStringStyle =
-                        layer.getDefaultLineStringStyle();
-                lineStringStyle.setColor(Color.RED);
-                lineStringStyle.setWidth(10f);
-
-                layer.addLayerToMap();
-            }
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // Called when the status of the provider changes
         }
-    }
 
+        @Override
+        public void onProviderEnabled(String provider) {
+            // Called when the provider is enabled
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            // Called when the provider is disabled
+        }
+    };
+
+    // Check if GPS is enabled
+    private boolean isGPSEnabled() {
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
 }
